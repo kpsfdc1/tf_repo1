7b0d900ed6c0e0c6808a29ea59e517a328e61eb4
root <root@ip-172-31-11-195.ap-northeast-1.compute.internal>
2023-11-07 05:34:56 +0000

    by user1

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+        region = "ap-northeast-1"
+}
+terraform {
+        backend "s3" {
+                bucket = "tf-backend-bucket-cis"
+                key = "terraform.tfstate"
+                region = "ap-northeast-1"
+        }
+}
+
+resource "aws_instance" "inst"{
+        ami = "ami-0ed99df77a82560e6"
+        instance_type = "t2.micro"
+}
+
